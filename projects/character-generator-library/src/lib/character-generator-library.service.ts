43bc250336fa56674e85ce4e09961a20d725a5fc
Chris Strong <christopher.strong@agvance.net>
2023-04-09 22:04:32 -0500

    Changed library

--- projects/character-generator-library/src/lib/character-generator-library.service.ts
+++ projects/character-generator-library/src/lib/character-generator-library.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -8,61 +8,41 @@ export class CharacterGeneratorLibraryService {
   constructor() { }
 
   public generateCharacter() {
-    const characterRaces = ['Human', 'Elf', 'Dwarf', 'Halfling', 'Dragonborn', 'Gnome', 'Half-Elf', 'Half-Orc', 'Tiefling'];
-    const characterClasses = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'];
-    const characterAbilities = ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma'];
-
-    const character = {
-      name: 'Unknown',
-      race: characterRaces[Math.floor(Math.random() * characterRaces.length)],
-      class: characterClasses[Math.floor(Math.random() * characterClasses.length)],
-      Strength: Math.floor(Math.random() * 18) + 1,
-      Dexterity: Math.floor(Math.random() * 18) + 1,
-      Constitution: Math.floor(Math.random() * 18) + 1,
-      Intelligence: Math.floor(Math.random() * 18) + 1,
-      Wisdom: Math.floor(Math.random() * 18) + 1,
-      Charisma: Math.floor(Math.random() * 18) + 1,
-      hitPoints: 0
+    const race = ['Human', 'Elf', 'Dwarf', 'Halfling', 'Dragonborn', 'Gnome', 'Half-Elf', 'Half-Orc', 'Tiefling'];
+    const classes = ['Artificer', 'Barbarian', 'Bard', "Blood-Hunter", 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'];
+    const abilityNames =['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma'];
+    const classToHPScale: {[key: string]: number} = {
+      'Artificer': 8,
+      'Barbarian': 12,
+      'Bard': 8,
+      'Blood-Hunter': 10,
+      'Cleric': 8,
+      'Druid': 8,
+      'Fighter': 10,
+      'Monk': 8,
+      'Paladin': 10,
+      'Ranger': 10,
+      'Rogue': 8,
+      'Sorcerer': 6,
+      'Warlock': 8,
+      'Wizard': 6
     };
 
-    switch (character.class) {
-      case 'Barbarian':
-        character.hitPoints = Math.floor(Math.random() * 12) + 1 + character.Constitution;
-        break;
-      case 'Bard':
-        character.hitPoints = Math.floor(Math.random() * 8) + 1 + character.Constitution;
-        break;
-      case 'Cleric':
-        character.hitPoints = Math.floor(Math.random() * 8) + 1 + character.Constitution;
-        break;
-      case 'Druid':
-        character.hitPoints = Math.floor(Math.random() * 8) + 1 + character.Constitution;
-        break;
-      case 'Fighter':
-        character.hitPoints = Math.floor(Math.random() * 10) + 1 + character.Constitution;
-        break;
-      case 'Monk':
-        character.hitPoints = Math.floor(Math.random() * 8) + 1 + character.Constitution;
-        break;
-      case 'Paladin':
-        character.hitPoints = Math.floor(Math.random() * 10) + 1 + character.Constitution;
-        break;
-      case 'Ranger':
-        character.hitPoints = Math.floor(Math.random() * 10) + 1 + character.Constitution;
-        break;
-      case 'Rogue':
-        character.hitPoints = Math.floor(Math.random() * 8) + 1 + character.Constitution;
-        break;
-      case 'Sorcerer':
-        character.hitPoints = Math.floor(Math.random() * 6) + 1 + character.Constitution;
-        break;
-      case 'Warlock':
-        character.hitPoints = Math.floor(Math.random() * 8) + 1 + character.Constitution;
-        break;
-      case 'Wizard':
-        character.hitPoints = Math.floor(Math.random() * 6) + 1 + character.Constitution;
-        break;
+    const randomRace = race[Math.floor(Math.random() * race.length)];
+    const randomClass = classes[Math.floor(Math.random() * classes.length)];
+    const constitution = Math.floor(Math.random() * 18) + 1;
+
+    const abilities: Record<string, number> = {};
+    abilityNames.forEach(ability => {
+      abilities[ability] = Math.floor(Math.random() * 18) + 1;
+    });
+
+    return {
+      name: 'Unknown',
+      race: randomRace,
+      class: randomClass,
+      abilities: abilities,
+      hitPoints: Math.floor(Math.random() * classToHPScale[randomClass]) + constitution
     }
-    return character;
   }
 }
